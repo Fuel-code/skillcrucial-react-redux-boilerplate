7932414cea61c96d624d4c461a5451fc7ee8b57a
admin <ssmilenko@gmail.com>
2020-09-20 22:02:08 +0300

    fck

--- client/components/home.js
+++ client/components/home.js
@@ -1,20 +1,35 @@
-import React, { useState } from 'react'
-import Head from './head'
-// import wave from '../assets/images/wave.jpg'
+import React, {useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getList } from '../redux/reducers/information'
 
 const Home = () => {
-  const [counter, setCounterNew] = useState(0)
+  const dispatch = useDispatch()
+  const list = useSelector((store) => store.info)
 
-  return (
-    <div>
-      <Head title="Hello" />
-      <img alt="wave" src="images/wave.jpg" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
-        updateCounter
-      </button>
-      <div> Hello World Dashboard {counter} </div>
-    </div>
+  useEffect(() => {
+    dispatch(getList())
+  }, [])
+
+  return <div>
+
+    {JSON.stringify(list)}
+  {list.map((d) =>(
+  <div key= {`${d.id}`}>
+  <div>{d.header}</div>
+  <div>{d.city}</div>
+  <div>{d.country}</div>
+  <div>{d.link}</div>
+  <div>{d.discription}</div>
+  <div>{d.tags}</div>
+  </div>
   )
+
+  )}
+
+
+
+  </div>
+
 }
 
 Home.propTypes = {}
